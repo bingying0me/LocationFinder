1a3a8c42892821342c84fe73a6decc80935b50d7
Ying <41935296+bingying0me@users.noreply.github.com>
2023-10-23 03:15:45 -0400

    Add files via upload

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <LocationFinder />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import LocationFinder from './components/LocationFinder.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    LocationFinder
   }
 }
 </script>
